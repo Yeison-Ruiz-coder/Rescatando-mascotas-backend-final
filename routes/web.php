c28acc0106eed8ebfba63cbfd6e42ec05c5f5508
Yeison-Ruiz-coder <yeisonruiz7777777@gmail.com>
2026-04-16 15:03:07 -0500

    Configuracion en web

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,3 @@ Route::get('/', function () {
         'status' => 'running'
     ]);
 });
-
-Route::get('/dashboard', function () {
-    return response()->json([
-        'message' => 'Dashboard',
-        'status' => 'running'
-    ]);
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
